d3aaab80e323f949900a650e43054a70564abb84
Sandeep <c.sandeepkumar@mulberri.io>
2024-05-19 13:58:39 +0530

    Minor change

--- src/layouts/common.tsx
+++ src/layouts/common.tsx
@@ -29,14 +29,14 @@ const CommonLayout = () => {
 									<path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
 								</svg>
 							</button>
-							<a href="https://flowbite.com" className="flex ms-12 md:me-24">
+							<a href="https://flowbite.com" className="flex md:ms-12 ms-2 md:me-24">
 								<span className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#E4A76F] to-[#F3BD85] self-center sm:text-2xl whitespace-nowrap">
 									TalAura
 								</span>
 							</a>
 						</div>
 						<div className="flex items-center">
-							<div className="flex items-center mr-8">
+							<div className="flex items-center md:mr-8">
 								<div className="flex items-center">
 									<button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
 										<span className="sr-only">Open user menu</span>
