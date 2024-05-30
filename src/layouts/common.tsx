769af7a0043afa3ab7ae724d9a0eda4365ae2dfe
Sandeep <c.sandeepkumar@mulberri.io>
2024-05-30 20:48:26 +0530

    config updated

--- src/layouts/common.tsx
+++ src/layouts/common.tsx
@@ -24,12 +24,6 @@ const CommonLayout = () => {
 				<div className="px-3 lg:px-5 lg:pl-3 flex w-full items-center h-16">
 					<div className="flex items-center justify-between w-full">
 						<div className="flex items-center justify-start rtl:justify-end">
-							<button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-								<span className="sr-only">Open sidebar</span>
-								<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-									<path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-								</svg>
-							</button>
 							<a href="/" className="flex md:ms-12 ms-2">
 								<span className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#E4A76F] to-[#F3BD85] self-center sm:text-2xl whitespace-nowrap">
 									TalAura
@@ -46,34 +40,9 @@ const CommonLayout = () => {
 										<span className="sr-only">Open user menu</span>
 										<img className="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" />
 									</button>
-									<p className="text-sm font-semibold text-gray-900 dark:text-white ml-3" role="none">
+									<p className="text-sm font-semibold text-gray-900 dark:text-white ml-2" role="none">
 										Neil Sims
 									</p>
-								</div>
-
-								<div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
-									<div className="px-4 py-3" role="none">
-										<p className="text-sm text-gray-900 dark:text-white" role="none">
-											Neil Sims
-										</p>
-										<p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-											neil.sims@gmail.com
-										</p>
-									</div>
-									<ul className="py-1" role="none">
-										<li>
-											<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
-										</li>
-										<li>
-											<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
-										</li>
-										<li>
-											<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
-										</li>
-										<li>
-											<a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
-										</li>
-									</ul>
 								</div>
 							</div>
 						</div>
--- src/pages/assessments/list.tsx
+++ src/pages/assessments/list.tsx
@@ -42,7 +42,7 @@ function MyAssessments () {
               <div className="relative w-full group">
                 <button className="py-2.5 px-3 w-full md:text-sm text-site bg-white border border-dimmed focus:border-brand focus:outline-none focus:ring-0 peer flex items-center justify-between rounded font-semibold">Active</button>
                 <div
-                  className="absolute z-[99] top-[100%] left-[50%] translate-x-[-50%] rounded-md overflow-hidden shadow-lg min-w-[150px] w-max peer-focus:visible peer-focus:opacity-100 opacity-0 invisible duration-200 p-1 bg-gray-100 dark:bg-gray-800  border border-dimmed text-xs md:text-sm">
+                  className="absolute z-[99] top-[100%] left-[50%] translate-x-[-50%] rounded-md overflow-hidden shadow-lg min-w-[150px] opacity-0 invisible w-max peer-focus:visible peer-focus:opacity-100 duration-200 p-1 bg-gray-100 dark:bg-gray-800  border border-dimmed text-xs md:text-sm">
                   <div
                     className=" w-full block cursor-pointer hover:bg-white dark:hover:bg-gray-900 dark:bg-gray-800 hover:text-link px-3 py-2 rounded-md">
                     All</div>
